89b6aa2d75c14aa5dc9323f39b5ba67f5f84dbc6
Brian Valdillez <valdillez+codebending@alumni.usc.edu>
2026-01-14 14:12:30 -0800

    [4.4] JSON: The Profane - Added a basic word filter

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -8,16 +8,35 @@ export async function handlerChirpValidation(req: Request, res: Response) {
 
    // req.body is automatically parsed
   const params: parameters = req.body;
-  const body:string = params?.body;
+  let body:string = params?.body;
   if (!body || typeof body !== 'string'){
     respondWithError(res, 400, "Invalid JSON");
     return;
   }
+
+  body = cleanMessage(body);
 
   if(body.length > 140){
     respondWithError(res, 400, "Chirp is too long");
     return;
   }
 
-  respondWithJSON(res, 200, { valid: true });    
+  respondWithJSON(res, 200, { cleanedBody: body });    
+}
+
+const PROFANE_REPLACEMENT = '****';
+const PROFANE_WORDS = ['kerfuffle', 'sharbert', 'fornax'];
+function cleanMessage(msg: string): string {
+  const words = msg.split(/\s+/);
+
+  for (const i in words){
+    const word = words[i];
+    console.log(`Word #${i}: ${word}`);
+    if (PROFANE_WORDS.includes(word.toLowerCase()))
+    {
+      words[i] = PROFANE_REPLACEMENT;
+    }
+  }
+
+  return words.join(' ');
 }
