b7fba00164a88a695f6fe783dc72c54041081f26
Brian Valdillez <valdillez+codebending@alumni.usc.edu>
2026-01-16 10:51:45 -0800

    [6.3] Storage: Drizzle Setup + Queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,21 @@
+import { env } from "node:process";
+
+process.loadEnvFile();
+
 type APIConfig = {
+    dbURL: string;
     fileserverHits: number;
 };
 
 export const config: APIConfig = {
+    dbURL: envOOrThrow('DB_URL'),
     fileserverHits: 0,
 };
+
+function envOOrThrow(key: string): string {
+    if (!process.env){
+        throw new Error(`env key not found: ${key}`);
+    }
+
+    return process.env[key] as string;
+}
